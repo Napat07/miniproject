e85e979ee59ab568fdeb88a7e8315325604c98ba
Napat07 <naphat1811@gmail.com>
2020-03-21 20:50:23 +0700

    add Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './components/Nav'
 function App() {
   return (
     <div>
-      
+      <Nav/>
     </div>
   );
 }
