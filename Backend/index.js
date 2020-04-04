adfae4aa26d7de2e24c5c7167f5d40f56e7a0fd6
Napat07 <naphat1811@gmail.com>
2020-04-05 00:55:05 +0700

    edit all

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const env = require('dotenv').config() 
-const soap = require('soap');
-const url = 'https://passport.psu.ac.th/authentication/authentication.asmx?wsdl';
-const app = express();
-const routes = require('./routes')
 
+
+const app = express();
 
 app.listen(process.env.PORT,() =>  console.log('server ready 80') );
 
@@ -69,58 +67,4 @@ app.delete('/api/countries/:country_id', (req, res) => { // ลบประเ�
   res.send(Countries);
   console.log('Delete Country', id);
 });
-
-
-const out = `
-<html>
-<style>
-body {
-  text-align: center;
-}
-</style>
-<body>
-<img src="https://www.computing.psu.ac.th/th/wp-content/uploads/2018/03/Logo-PSU-TH-01.png" width="450" height="270"/>
-  <h2>PSU Passport Authentication (SOAP) </h2>
- <form action="/psupassport" method="post">
- Username: <input type="text" name="username" /> <br>
- Password: <input type="password" name="password" /> <br>
- <input type="submit" value="Submit">
-</form>
-</body>
-</html> 
-`
-app.get('/psupassport', (req, res) => {
-   res.send(out)
-})
-app.post('/psupassport', (req, res) => {
-   soap.createClient(url, (err, client) => {
-      if (err) {
-         res.redirect("http://localhost:8055")
-      }
-      else {
-         let user = {}
-         user.username = req.body.username
-         user.password = req.body.password
-
-         client.GetStaffDetails(user, function (err, response) {
-
-            if (err) {
-               res.redirect("http://localhost:8055")
-            }
-            else {
-               console.log(response);
-               if (response.GetStaffDetailsResult.string[0]) {
-                  req.session.access_token = '123'
-                  req.session.expires = 60000
-                  res.redirect("http://localhost:3000/#total")
-               } else {
-                  res.redirect("http://localhost:8055")
-               }
-            }
-         });
-      }
-   });
-})
-
-
 
