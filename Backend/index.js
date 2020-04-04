7507f3138ee06ca3a57cfce34894592e1501e772
Napat07 <naphat1811@gmail.com>
2020-04-05 00:33:31 +0700

    install soap

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const env = require('dotenv').config() 
+const soap = require('soap');
+
 
 
 const app = express();
