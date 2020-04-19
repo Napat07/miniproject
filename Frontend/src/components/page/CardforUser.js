33659c1b1ec166acd0333c4e74f91fc510c1162f
Napat07 <naphat1811@gmail.com>
2020-04-20 02:25:10 +0700

    delete small details

--- Frontend/src/components/page/CardforUser.js
+++ Frontend/src/components/page/CardforUser.js
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CardforUser = props => {
 
-    const dispatch = useDispatch()
-    const form = useSelector(state => state.form)
 
     return (
         <div className='productcard-container'>
